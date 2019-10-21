3f305ee588133bfb0d4893e50de25e7042f8acf6
John Heath <john@Beast.heath.mx>
2019-10-21 19:57:58 +0100

    added locking

--- malloc_override.cpp
+++ malloc_override.cpp
@@ -3,7 +3,6 @@
 #include <atomic>
 #include <dlfcn.h>
 #include <cinttypes>
-#include <mutex>
 
 //make this easy - typedefs make things clear
 typedef void* (*real_malloc_t)(size_t);
@@ -23,31 +22,40 @@ std::atomic<long long> callocated (0);
 std::atomic<long long> rallocated (0);
 
 //init lock
-std::mutex init_lock;
+std::atomic<bool> init_flag(false);
+std::atomic_flag lock = ATOMIC_FLAG_INIT;
 
 //TODO: need to add a way to record the deletions (hashmap or potentially simply write in another thread )
 std::atomic<long long> freed (0);
 
 static FILE *memlog = 0;
 
-#define LOCK ;
-#define UNLOCK ;
-
 //helper functions 
 static void initialize_symbols()
 {
-    std::lock_guard<std::mutex> guard(init_lock);
-    if (memlog)
-        return;
+    //return is another thread initialises it,
+    //otherwise grab the lock and initialise
+    do
+    {
+        if ( init_flag.load() )
+            return;
+    } while(std::atomic_flag_test_and_set_explicit(&lock, std::memory_order_acquire));
+
+    
+
+             ; // spin until the lock is acquired
     memlog = fdopen(200, "w");
     if (!memlog)
     {
         memlog = stderr;
     }
+    fprintf(memlog, "Setting symbols\n");
     real_malloc = reinterpret_cast<real_malloc_t>( dlsym(RTLD_NEXT, "malloc") );
     real_calloc = reinterpret_cast<real_calloc_t>( dlsym(RTLD_NEXT, "calloc") );
     real_free = reinterpret_cast<real_free_t>( dlsym(RTLD_NEXT, "free") );
     real_realloc = reinterpret_cast<real_realloc_t> (dlsym(RTLD_NEXT, "realloc") );
+    init_flag = true;
+    std::atomic_flag_clear_explicit(&lock, std::memory_order_release);
 }
 
 
@@ -66,12 +74,10 @@ void
 {
     if (memlog)
     {
-        LOCK;
         fprintf(memlog, "malloc %16" PRId64 " bytes\n", allocated.load());
         fprintf(memlog, "calloc %16" PRId64 " bytes\n", callocated.load());
         fprintf(memlog, "realloc %15" PRId64 " bytes\n", rallocated.load());
         //fprintf(memlog, "free %15" PRId64 " bytes\n", freed.load()); 
-        UNLOCK;
     }
 }
 
@@ -81,7 +87,7 @@ void
 void *malloc(size_t size)
 {
     void *p;
-    if (!real_malloc)
+    while(!init_flag.load())
     {
         initialize_symbols();
         if (!real_malloc)
@@ -96,7 +102,7 @@ void *malloc(size_t size)
 void *calloc(size_t nmemb, size_t size)
 {
     void *p;
-    if (!real_calloc)
+    while(!init_flag.load())
     {
         initialize_symbols();
         if (!real_calloc)
@@ -109,7 +115,7 @@ void *calloc(size_t nmemb, size_t size)
 
 void free(void *ptr)
 {
-    if (!real_free)
+    while(!init_flag.load())
     {
         initialize_symbols();
         if (!real_free)
@@ -122,7 +128,7 @@ void free(void *ptr)
 void *realloc(void *ptr, size_t size)
 {
     void *p;
-    if (!real_realloc)
+    while(!init_flag.load())
     {
         initialize_symbols();
         if (!real_realloc)
